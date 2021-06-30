48d53ccf8f735e4b34a034dd15331f67b6344207
The3Legend <durdvlad@gmail.com>
2021-06-30 21:20:46 +0300

    fix

--- src/js/helpers/validate.js
+++ src/js/helpers/validate.js
@@ -1,9 +1,9 @@
 const regExpDic = {
   email: /^[^ ]+@[^ ]+\.[a-z]{2,3}$/,
-  password: /^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z]).{4,}$/,
+  password: /^[0-9a-zA-Z](4,)$/,
 };
 
 export function validate(el) {
   const regExpName = el.dataset.required;
-  console.log(regExpName)
+  if (!regExpDic[regExpName]) return true;
 }
