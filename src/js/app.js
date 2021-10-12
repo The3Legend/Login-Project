c6d9360010de07b9f52b0c57ccbbe1a5b0d17a45
The3Legend <durdvlad@gmail.com>
2021-10-12 16:42:41 +0300

    fix and start new

--- src/js/app.js
+++ src/js/app.js
@@ -2,13 +2,8 @@ import "bootstrap/dist/css/bootstrap.css";
 import "../css/style.css";
 
 import UI from "./config/ui.config";
-import { validate } from "./helpers/validate";
-import { showInputError, removeInputError } from "./views/form";
-import { login } from "./services/auth.services";
-import { notify} from "./views/notifications";
 
 const { form, inputEmail, inputPassword } = UI;
-const inputs = [inputEmail, inputPassword];
 
 //Events
 form.addEventListener("submit", (e) => {
@@ -16,29 +11,5 @@ form.addEventListener("submit", (e) => {
   onSubmit();
 });
 
-inputs.forEach((el) => {
-  el.addEventListener("focus", () => {
-    removeInputError(el);
-  });
-});
-//Handler
-
-async function onSubmit() {
-  const isValidForm = inputs.every((el) => {
-    const isValidInput = validate(el);
-    if (!isValidInput) {
-      showInputError(el);
-    }
-    return isValidInput;
-  });
-
-  console.log(isValidForm);
-  if (!isValidForm) return;
-  try {
-    await login(inputEmail.value, inputPassword.value);
-    form.reset();
-    notify({ mas: "Login success", className: "alert-success" });
-  } catch (error) {
-    notify({ mas: "Login faild", className: "alert-danger" });
-  }
-}
+//Hendlers
+function onSubmit() {}
